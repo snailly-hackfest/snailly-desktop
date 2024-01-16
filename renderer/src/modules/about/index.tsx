aaaf10221c57554fc8ce255c249f1bbdb5a87630
Raihan Syah <raihansyah45@gmail.com>
2024-01-16 13:33:01 +0700

    feat:add about module

---
+++ renderer/src/modules/about/index.tsx
@@ -0,0 +1,32 @@
+import { sAbout, sAboutLogo, sAboutDodo } from "./styles";
+
+import { LogoSnailyGreen } from "@/assets";
+import { Paragraph } from "@/typography";
+import { DodoLoginSmall } from "@/assets";
+
+const AboutModule = () => {
+    return (
+        <>
+            <div className={sAbout}>
+                <div className={sAboutLogo}>
+                    <LogoSnailyGreen />
+                </div>
+                <Paragraph variant="s" weight="normal" darkGreen>
+                    Snailly: Safe browsing for the children <br />
+                    version 1.0 &copy;<br />
+                    Copyright 2023 UNIKOM CODELABS. <br />
+                    All rights reserved. <br />
+                </Paragraph>
+                <Paragraph variant="s" weight="normal" darkGreen>
+                    Snailly adalah sebuah aplikasi untuk para orang tua mengontrol dan mengawasi aktivitas anak-anak mereka dalam menggunakan internet, di mana anak-anak dapat mengeksplorasi dunia internet dengan aman dan orang tua tidak akan merasa
+                    khawatir tentang bahaya dari internet.
+                </Paragraph>
+                <div className={sAboutDodo}>
+                    <DodoLoginSmall />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AboutModule;
---
+++ renderer/src/modules/about/styles.ts
@@ -0,0 +1,30 @@
+import { css } from "@emotion/css";
+
+
+export const sAbout = css`
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const sAboutHeading = css`
+  padding-top: 7px;
+`
+
+export const sAboutLogo = css`
+    display: flex;
+    justify-content: center;
+    height: 150px;
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 10px;
+`
+
+export const sAboutDodo = css`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  height: 35vh;
+  z-index: -1;
+`
