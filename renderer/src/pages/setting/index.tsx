76440eb9e999c93306119d1a9a15b5a45d8706ce
Raihan Syah <raihansyah45@gmail.com>
2024-01-16 13:28:04 +0700

    feat:add setting page

---
+++ renderer/src/pages/setting/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { PageWrapper } from "@/layout";
+import { SettingModule } from "@/modules";
+
+const setting = () => {
+  return (
+    <PageWrapper layoutType="base" title="Pengaturan">
+      <SettingModule />
+    </PageWrapper>
+  );
+};
+
+export default setting;
